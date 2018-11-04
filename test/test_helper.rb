83ac37ab74fccc615c5ca5403993f67e851eb1ec
Bill Niblock <niblock@oath.com>
2018-11-04 12:34:43 -0500

    Futz around with tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,8 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "scrivnr"
 
 require "minitest/autorun"
+require "minitest/pride"
+require "scrivnr"
+
+# Disable the logger
+Scrivnr::Ledger.disable
